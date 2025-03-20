b2a9c96c660e3a2b856c0d95a1bd90f56c6de125
Marcell Kőműves <kommarci03@gmail.com>
2025-03-20 12:59:27 +0100

    bug methods implementation pt1

--- Code/Model/Bug/Bug.java
+++ Code/Model/Bug/Bug.java
@@ -1,6 +1,7 @@
 package Bug;
 
 import Controll.Player;
+import Controll.Skeleton;
 import Shroomer.Hypa;
 import Shroomer.Spore;
 import Tekton.Tekton;
@@ -23,36 +24,96 @@ public class Bug extends Player {
     /**
      * Default constructor
      */
-    public Bug() {
-    }
-
-    /**
-     * @param s
-     */
-    public void setStrategy(Strategy s) {
-        // TODO implement here
-    }
+    public Bug() {}
 
     /**
      * @param to
      */
     public void move(Tekton to) {
-        // TODO implement here
+        Skeleton.SKELETON.callStackDepth++;
+        Skeleton.SKELETON.printCall(Skeleton.SKELETON.objectNameMap.get(this)+"->"+Skeleton.SKELETON.objectNameMap.get(strategy)+": move("+
+                Skeleton.SKELETON.objectNameMap.get(this)+", "+Skeleton.SKELETON.objectNameMap.get(strategy)+")");
+
+        boolean canDo = strategy.move(this, to);
+
+        Skeleton.SKELETON.printCall(Skeleton.SKELETON.objectNameMap.get(strategy)+"-->>"+Skeleton.SKELETON.objectNameMap.get(this)+": "+canDo);
+
+        if (canDo) {
+            Skeleton.SKELETON.printCall(Skeleton.SKELETON.objectNameMap.get(this)+"->"+Skeleton.SKELETON.objectNameMap.get(to)+": tryBug("+
+                    Skeleton.SKELETON.objectNameMap.get(this)+")");
+            to.tryBug(this);
+            Skeleton.SKELETON.printCall(Skeleton.SKELETON.objectNameMap.get(to)+"-->>"+Skeleton.SKELETON.objectNameMap.get(this));
+
+            Skeleton.SKELETON.printCall(Skeleton.SKELETON.objectNameMap.get(this)+"->"+Skeleton.SKELETON.objectNameMap.get(tekton)+": setBug(null)");
+            tekton.setBug(null);
+            Skeleton.SKELETON.printCall(Skeleton.SKELETON.objectNameMap.get(tekton)+"-->>"+Skeleton.SKELETON.objectNameMap.get(this));
+
+            Skeleton.SKELETON.printCall(Skeleton.SKELETON.objectNameMap.get(this)+"->"+Skeleton.SKELETON.objectNameMap.get(this)+": setLocation("+
+                    Skeleton.SKELETON.objectNameMap.get(to)+")");
+            setLocation(to);
+            Skeleton.SKELETON.printCall(Skeleton.SKELETON.objectNameMap.get(this)+"-->>"+Skeleton.SKELETON.objectNameMap.get(this));
+        }
+
+        Skeleton.SKELETON.callStackDepth--;
     }
 
     /**
      * @param h
      */
     public void bite(Hypa h) {
-        // TODO implement here
+        Skeleton.SKELETON.callStackDepth++;
+
+        Skeleton.SKELETON.printCall(Skeleton.SKELETON.objectNameMap.get(this)+"->"+Skeleton.SKELETON.objectNameMap.get(strategy)+": bite("+
+                Skeleton.SKELETON.objectNameMap.get(this)+", "+Skeleton.SKELETON.objectNameMap.get(h)+")");
+
+        boolean canDo = strategy.bite(this, h);
+
+        Skeleton.SKELETON.printCall(Skeleton.SKELETON.objectNameMap.get(strategy)+"-->>"+Skeleton.SKELETON.objectNameMap.get(this)+": "+canDo);
+
+        if (canDo) {
+            Skeleton.SKELETON.printCall(Skeleton.SKELETON.objectNameMap.get(this)+"->"+Skeleton.SKELETON.objectNameMap.get(h)+": die()");
+
+            h.die();
+
+            Skeleton.SKELETON.printCall(Skeleton.SKELETON.objectNameMap.get(h)+"->"+Skeleton.SKELETON.objectNameMap.get(this));
+        }
+
+        Skeleton.SKELETON.callStackDepth--;
     }
 
     /**
      * @param s
      */
     public void eat(Spore s) {
-        // TODO implement here
+        Skeleton.SKELETON.callStackDepth++;
+
+        Skeleton.SKELETON.printCall(Skeleton.SKELETON.objectNameMap.get(this)+"->"+Skeleton.SKELETON.objectNameMap.get(strategy)+": eat("+
+                Skeleton.SKELETON.objectNameMap.get(this)+", "+Skeleton.SKELETON.objectNameMap.get(s)+")");
+
+
     }
 
-    public Tekton getLocation(){return tekton;}
+    /**
+     *
+     * @return
+     */
+
+    public Tekton getLocation(){
+        return tekton;
+    }
+
+    /**
+     * @param newLoc
+     */
+
+    public void setLocation(Tekton newLoc){
+        this.tekton = newLoc;
+    }
+
+    /**
+     * @param s
+     */
+    public void setStrategy(Strategy s) {
+        this.strategy = s;
+    }
 }
