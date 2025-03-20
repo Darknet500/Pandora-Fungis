68a124565cfce900aec3ea0c6c408461d7100074
Marcell Kőműves <kommarci03@gmail.com>
2025-03-20 13:12:54 +0100

    bug methods implemented pt2

--- Code/Model/Bug/Bug.java
+++ Code/Model/Bug/Bug.java
@@ -5,6 +5,7 @@ import Controll.Skeleton;
 import Shroomer.Hypa;
 import Shroomer.Spore;
 import Tekton.Tekton;
+import java.util.List;
 
 /**
  * 
@@ -30,67 +31,34 @@ public class Bug extends Player {
      * @param to
      */
     public void move(Tekton to) {
-        Skeleton.SKELETON.callStackDepth++;
-        Skeleton.SKELETON.printCall(Skeleton.SKELETON.objectNameMap.get(this)+"->"+Skeleton.SKELETON.objectNameMap.get(strategy)+": move("+
-                Skeleton.SKELETON.objectNameMap.get(this)+", "+Skeleton.SKELETON.objectNameMap.get(strategy)+")");
-
         boolean canDo = strategy.move(this, to);
-
-        Skeleton.SKELETON.printCall(Skeleton.SKELETON.objectNameMap.get(strategy)+"-->>"+Skeleton.SKELETON.objectNameMap.get(this)+": "+canDo);
-
         if (canDo) {
-            Skeleton.SKELETON.printCall(Skeleton.SKELETON.objectNameMap.get(this)+"->"+Skeleton.SKELETON.objectNameMap.get(to)+": tryBug("+
-                    Skeleton.SKELETON.objectNameMap.get(this)+")");
             to.tryBug(this);
-            Skeleton.SKELETON.printCall(Skeleton.SKELETON.objectNameMap.get(to)+"-->>"+Skeleton.SKELETON.objectNameMap.get(this));
-
-            Skeleton.SKELETON.printCall(Skeleton.SKELETON.objectNameMap.get(this)+"->"+Skeleton.SKELETON.objectNameMap.get(tekton)+": setBug(null)");
             tekton.setBug(null);
-            Skeleton.SKELETON.printCall(Skeleton.SKELETON.objectNameMap.get(tekton)+"-->>"+Skeleton.SKELETON.objectNameMap.get(this));
-
-            Skeleton.SKELETON.printCall(Skeleton.SKELETON.objectNameMap.get(this)+"->"+Skeleton.SKELETON.objectNameMap.get(this)+": setLocation("+
-                    Skeleton.SKELETON.objectNameMap.get(to)+")");
             setLocation(to);
-            Skeleton.SKELETON.printCall(Skeleton.SKELETON.objectNameMap.get(this)+"-->>"+Skeleton.SKELETON.objectNameMap.get(this));
         }
-
-        Skeleton.SKELETON.callStackDepth--;
     }
 
     /**
      * @param h
      */
     public void bite(Hypa h) {
-        Skeleton.SKELETON.callStackDepth++;
-
-        Skeleton.SKELETON.printCall(Skeleton.SKELETON.objectNameMap.get(this)+"->"+Skeleton.SKELETON.objectNameMap.get(strategy)+": bite("+
-                Skeleton.SKELETON.objectNameMap.get(this)+", "+Skeleton.SKELETON.objectNameMap.get(h)+")");
-
         boolean canDo = strategy.bite(this, h);
-
-        Skeleton.SKELETON.printCall(Skeleton.SKELETON.objectNameMap.get(strategy)+"-->>"+Skeleton.SKELETON.objectNameMap.get(this)+": "+canDo);
-
-        if (canDo) {
-            Skeleton.SKELETON.printCall(Skeleton.SKELETON.objectNameMap.get(this)+"->"+Skeleton.SKELETON.objectNameMap.get(h)+": die()");
-
-            h.die();
-
-            Skeleton.SKELETON.printCall(Skeleton.SKELETON.objectNameMap.get(h)+"->"+Skeleton.SKELETON.objectNameMap.get(this));
-        }
-
-        Skeleton.SKELETON.callStackDepth--;
+        if (canDo) { h.die(); }
     }
 
     /**
      * @param s
      */
     public void eat(Spore s) {
-        Skeleton.SKELETON.callStackDepth++;
-
-        Skeleton.SKELETON.printCall(Skeleton.SKELETON.objectNameMap.get(this)+"->"+Skeleton.SKELETON.objectNameMap.get(strategy)+": eat("+
-                Skeleton.SKELETON.objectNameMap.get(this)+", "+Skeleton.SKELETON.objectNameMap.get(s)+")");
-
-
+        boolean canDo = strategy.eat(this, s);
+        if (canDo) {
+            List<Spore> spores = tekton.getStoredSpores();
+            if(spores.contains(s)){
+                s.haveEffect(this);
+                tekton.removeSpore(s);
+            }
+        }
     }
 
     /**
